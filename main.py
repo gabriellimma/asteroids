dc26dafed8a5af7459c0e0544d9a8b36e97926b0
Gabriel Lima <nickspoot@gmail.com>
2026-03-07 01:43:42 -0300

    add circle shape

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ def main():
     print(f"Starting Asteroids with pygame version: {VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    FPS = 60
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,6 +23,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip
+        dt = clock.tick(FPS) / 1000
 
 if __name__ == "__main__":
     main()
