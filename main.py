f4d923dc14ebdaf3f8060d57944d6aa80b801aef
Gabriel Lima <nickspoot@gmail.com>
2026-03-08 00:50:11 -0300

    add move, rotate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 VERSION = pygame.version.ver
 
@@ -9,20 +10,22 @@ def main():
     print(f"Starting Asteroids with pygame version: {VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    FPS = 60
-    
+    p1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip
+        p1.draw(screen)
+        p1.update(dt)
+        pygame.display.flip()
         dt = clock.tick(FPS) / 1000
 
 if __name__ == "__main__":
